19a57e629584cf06d2b4cdfb9efb83a0e57d5b63
jaked122 <jaked122@gmail.com>
2014-04-13 18:49:51 -0400

    Fixed all of the issues.

--- src/quorum/announce.go
+++ src/quorum/announce.go
@@ -1,58 +1,75 @@
 package quorum
 
-import "common"
 import "common/log"
 import "network"
 import "encoding/json"
+import "common/erasure"
 import "os"
+import "io"
+import "fmt"
 
-type Announce struct{
-	ID string
-	Size int64
-	Chunks int16
+type Announce struct {
+	ID     string
+	Size   int
+	Chunks int
 }
 
-func CreateFileAnnounce(file string) *Announce{
-	A:=new(Announce)
-	f,err:=os.Open(file)
-	if err!=nil{
-		panic(err.Error())
+func CreateFileAnnounce(file string) *Announce {
+	A := new(Announce)
+	f, err := os.Open(file)
+	if err != nil {
+		log.Fatal(err)
 	}
-	c,err:=f.Stat()
-	if err!=nil{
+	defer f.Close()
+	c, err := f.Stat()
+	if err != nil {
 	}
-	A.Size=c.Size()
-	A.Chunks=15;
+	A.Size = int(c.Size())
+	//this should be enough.
+	A.Chunks = A.Size / (1 << 20)
 	//Chunk File
+	d := make([]byte, A.Size/(1<<20))
+	indexs := 0
+	//generate ID and dump the chunks into files
+	//A.ID=""
+	for _ = 0; err != io.EOF; _, err = f.Read(d) {
+		if err != nil {
+		}
+		redunt, _ := erasure.EncodeRing(A.Chunks, A.Size/A.Chunks, d)
+		indexs = indexs + 1
+		for index, val := range redunt {
+			k, e := os.Open(fmt.Sprintf("%s-%d%d", A.ID, indexs, index))
+			if e != nil {
+				log.Fatal(e)
+			}
+			if _, e = k.Write([]byte(val)); e != nil {
+				log.Fatal(e)
+			}
+		}
+	}
+
 	return A
 }
 
-func (a *Announce) SendOutAnnounce(recipients []*Participant) *Error{
+func (a *Announce) Type() string {
+	return "Announce"
+}
+
+func (a *Announce) SendOutAnnounce(recipients []*Participant) error {
 	//TODO Add code to actually send out to participants when it works.
-	for i :=range( recipients){
-		resp,err:=network.SendMessage(i.Address,i.Port,[]byte(json.Encode(a)))
-		if err!=nil{
-			panic(err.Error())
+	for _, i := range recipients {
+		s, err := json.Marshal(a)
+		if err != nil {
+			log.Fatal(err)
 		}
-		if resp[0]>0{
+		resp, err := network.SendMessage(i.Address, i.Port, []byte(s))
+		if err != nil {
+			log.Fatal(err)
+		}
+		if resp[0] > 0 {
 			//handle file transmission, passing chunk resp[1]
 		}
 	}
-		
+
 	return nil
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/quorum/state.go
+++ src/quorum/state.go
@@ -40,8 +40,8 @@ type State struct {
 // more fields might be added.
 type Participant struct {
 	PublicKey crypto.PublicKey
-	Address string
-	Port int16
+	Address   string
+	Port      int
 }
 
 // Create and initialize a state object
